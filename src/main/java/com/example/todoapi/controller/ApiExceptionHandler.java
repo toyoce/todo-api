0e43077a8fc977c2019cfdc70e8e740beed2463d
toyoce <toyo.naoki0701@gmail.com>
2022-12-15 21:56:01 +0900

    デフォルトの例外ハンドラを追加

--- src/main/java/com/example/todoapi/controller/ApiExceptionHandler.java
+++ src/main/java/com/example/todoapi/controller/ApiExceptionHandler.java
@@ -3,17 +3,24 @@ package com.example.todoapi.controller;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.example.todoapi.exception.ApiError;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         ApiError apiError = new ApiError(status.value(), ex.getMessage());
         return super.handleExceptionInternal(ex, apiError, headers, status, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
+        ApiError apiError = new ApiError(500, "Internal Server Error");
+        return handleExceptionInternal(ex, apiError, null, HttpStatusCode.valueOf(500), request);
     }
 }
